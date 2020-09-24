ce9d6928e395d4864fde714ce91ac16c98d10779
nicolas medrano soto <nicolas@MacBook-Pro-de-nicolas-2.local>
2020-09-24 00:06:34 -0300

    se agrega splashscreen en android e ios

--- beetrack/App.js
+++ beetrack/App.js
@@ -1,13 +1,21 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Provider } from 'react-redux';
 import TabNavigator from './src/navigation/tabNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import configureStore from './store';
+import SplashScreen from 'react-native-splash-screen';
 
 const store = configureStore();
 
+
+
 const App: () => React$Node = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  },[])
+
   return (
     <Provider store={store}>
       <NavigationContainer>
