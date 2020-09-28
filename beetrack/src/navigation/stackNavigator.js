f81f4aeabe5c02aa42509598e4d6fdab24f34bea
nicolas medrano soto <nicolas@MacBook-Pro-de-nicolas-2.local>
2020-09-28 17:21:01 -0300

    se agrega color a los headers

--- beetrack/src/navigation/stackNavigator.js
+++ beetrack/src/navigation/stackNavigator.js
@@ -10,8 +10,8 @@ export default function StackNavigator() {
     return (
         <Stack.Navigator
             initialRoutName="Home">
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Noticia" component={Noticia} options={({route}) => ({title: route.params.item.title})}/>
+            <Stack.Screen name="Home" component={Home} options={{headerStyle:{backgroundColor: '#28C1FF'}}}/>
+            <Stack.Screen name="Noticia" component={Noticia} options={({route}) => ({title: route.params.item.title, headerStyle:{backgroundColor: '#28C1FF'}})}/>
         </Stack.Navigator>
     )
 }
